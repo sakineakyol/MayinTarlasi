a03f415ce833e4f56282a1705686e36423997fd7
SAKINE\SAKINE <sakineeakyoll@gmail.com>
2022-07-13 21:25:41 +0300

    Add project files.

---
+++ MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmYeniOyun = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmKolay = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmOrta = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmZor = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmYeniOyun});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmYeniOyun
+            // 
+            this.tsmYeniOyun.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmKolay,
+            this.tsmOrta,
+            this.tsmZor});
+            this.tsmYeniOyun.Name = "tsmYeniOyun";
+            this.tsmYeniOyun.Size = new System.Drawing.Size(73, 20);
+            this.tsmYeniOyun.Text = "Yeni Oyun";
+            // 
+            // tsmKolay
+            // 
+            this.tsmKolay.Name = "tsmKolay";
+            this.tsmKolay.Size = new System.Drawing.Size(180, 22);
+            this.tsmKolay.Text = "Kolay";
+            this.tsmKolay.Click += new System.EventHandler(this.tsmKolay_Click);
+            // 
+            // tsmOrta
+            // 
+            this.tsmOrta.Name = "tsmOrta";
+            this.tsmOrta.Size = new System.Drawing.Size(180, 22);
+            this.tsmOrta.Text = "Orta";
+            this.tsmOrta.Click += new System.EventHandler(this.tsmOrta_Click);
+            // 
+            // tsmZor
+            // 
+            this.tsmZor.Name = "tsmZor";
+            this.tsmZor.Size = new System.Drawing.Size(180, 22);
+            this.tsmZor.Text = "Zor";
+            this.tsmZor.Click += new System.EventHandler(this.tsmZor_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 39);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(775, 399);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmYeniOyun;
+        private System.Windows.Forms.ToolStripMenuItem tsmKolay;
+        private System.Windows.Forms.ToolStripMenuItem tsmOrta;
+        private System.Windows.Forms.ToolStripMenuItem tsmZor;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ MayinTarlasi/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmKolay_Click(object sender, EventArgs e)
+        {
+            TarlaOlustur(10, 10);
+        }
+        void TarlaOlustur(int satir, int sutun)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int mayin = (satir * sutun) / 10;
+
+            int[] mayinlar = new int[mayin];
+
+            Random rnd = new Random();
+            for (int i = 0; i < mayinlar.Length; i++) //int i = 0; i < mayin; i++
+            {
+                int secilen = rnd.Next(0, satir * sutun);
+                if (mayinlar.Contains(secilen) == true)
+                {
+                    i--;
+                }
+                else
+                {
+                    mayinlar[i] = secilen;
+                }
+            }
+
+            for (int i = 0; i < satir * sutun; i++)
+            {
+                Button btn = new Button();
+                btn.Width = btn.Height = 30;
+                btn.BackColor = Color.Green;
+                if (mayinlar.Contains(i) == true)
+                    btn.Tag = true;
+                else
+                    btn.Tag = false;
+                btn.Click += Btn_Click;
+                btn.Margin = new Padding(5, 5, 0, 0);
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+            flowLayoutPanel1.Width = sutun * 35;
+            flowLayoutPanel1.Height = satir * 35;
+            this.Width = flowLayoutPanel1.Width + 40;
+            this.Height = flowLayoutPanel1.Height + 80;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button tiklanan = (Button)sender; // sender as Button; 2 farklı cast yöntemi
+            if ((bool)tiklanan.Tag == true) //bool gelmezse hata verir
+            {
+                foreach (Control item in flowLayoutPanel1.Controls)
+                {
+                    Button btn = item as Button;
+                    if ((bool)btn.Tag == true)
+                    {
+                        btn.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.Green;
+                    }
+                    
+                }
+                MessageBox.Show("Oyun Bitti. Yeni Oyun Başlatın");
+                flowLayoutPanel1.Controls.Clear();
+            }
+            else
+            {
+                tiklanan.BackColor = Color.Gray;
+            }
+
+        }
+
+        private void tsmOrta_Click(object sender, EventArgs e)
+        {
+            TarlaOlustur(13, 13);
+        }
+
+        private void tsmZor_Click(object sender, EventArgs e)
+        {
+            TarlaOlustur(16, 16);
+        }
+    }
+}
